d4570e2ce519eabe3d4e64f3236204f2f51bf2f6
Kirikami <naumenko.ruslan@outlook.com>
2017-12-15 15:33:14 +0700

    Update user.go

--- models/user.go
+++ models/user.go
@@ -8,16 +8,16 @@ type User struct {
 	SBasicModel
 	Timer          Timer
 
-	ReferralId     int64    `json:"referral_id"`
-	NickName       string   `json:"nick_name"`
-	FirstName      string   `json:"first_name"`
-	LastName       string   `json:"last_name"`
-	AvatarId       string   `json:"avatar_id"`
-	Age            int      `json:"age"`
-	Email          string   `json:"email"`
-	EmailVerified  bool     `json:"email_verified"`
-	Groups    []string `json:"communities"`
-	Referral       *int64   `json:"referral"`
+	ReferralId
+	NickName  
+	FirstName 
+	LastName  
+	AvatarId  
+	Age       
+	Email     
+	EmailVerified 
+	Groups    
+	Referral  
 }
 
 type Iteration struct {
@@ -27,4 +27,9 @@ type Iteration struct {
 	Completed bool
 }
 
-
+type Group struct {
+	SBasicModel
+	OwnerID
+	Title
+	Description
+}
